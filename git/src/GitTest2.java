08a8b314e6844cbbb4fb2c83abbc1e15b9d6d441
snowwolf0026 <1938398325@qq.com>
2017-12-05 19:32:06 +0800

    up git2

---
+++ git/src/GitTest2.java
@@ -0,0 +1,10 @@
+public class GitTest2 {
+    public static void main(String args[]){
+        new GitTest2().play();
+
+    }
+
+    public void play(){
+        System.out.println("坦克世界");
+    }
+}
